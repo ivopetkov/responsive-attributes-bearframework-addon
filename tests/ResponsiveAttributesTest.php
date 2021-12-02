c596eb35e0e2ed30ea744cd7faad0f22eed27b38
Ivo Petkov <ivo@ivopetkov.com>
2021-12-02 14:19:11 +0200

    Update ResponsiveAttributesTest.php

--- tests/ResponsiveAttributesTest.php
+++ tests/ResponsiveAttributesTest.php
@@ -19,6 +19,10 @@ class ResponsiveAttributesTest extends BearFramework\AddonTests\PHPUnitTestCase
     public function testOutput()
     {
         $app = $this->getApp();
-        $this->assertTrue(true);
+
+        $html = '<html><head><link rel="client-packages-embed" name="responsiveAttributes"></head></html>';
+        $result = $app->clientPackages->process($html);
+
+        $this->assertTrue(strpos($result, '<script>var responsiveAttributes=') !== false);
     }
 }
